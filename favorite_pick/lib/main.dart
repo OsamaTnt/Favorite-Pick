93f1d6dea1b01cc181832db634b5413a59536cf3
osamatnt <osamatantush@gmail.com>
2021-12-25 18:37:04 +0200

    screen1 "Choose sports" implemented.

--- favorite_pick/lib/main.dart
+++ favorite_pick/lib/main.dart
@@ -19,54 +19,18 @@ class FavoritePick extends StatelessWidget {
       providers: [ Provider(create: (_) => Data()), ],
       child: ScreenUtilInit(
         designSize: const Size(360, 640),
-        builder: () => const MaterialApp(
+        builder: () => MaterialApp(
           title: 'Favorite Pick',
-          home: HomeScreen(),
+          routes: {
+            '/': (context) =>  const HomeScreen(),
+            '/football': (context) =>  const HomeScreen(),
+            '/basketball': (context) =>  const HomeScreen(),
+            '/iceHockey': (context) =>  const HomeScreen(),
+            '/baseball': (context) =>  const HomeScreen(),
+          },
+          initialRoute: '/',
         ),
       ),
     );
   }
 }
-
-// class MyHomePage extends StatefulWidget {
-//   const MyHomePage({Key? key, required this.title}) : super(key: key);
-//
-//   final String title;
-//
-//   // @override
-//   // State<MyHomePage> createState() => _MyHomePageState();
-// }
-
-// class _MyHomePageState extends State<MyHomePage> {
-
-  // void _incrementCounter() {
-  //   setState(() {
-  //     _counter++;
-  //   });
-  // }
-
-    // return Scaffold(
-    //   appBar: AppBar(
-    //     title: Text(widget.title),
-    //   ),
-    //   body: Center(
-    //     child: Column(
-    //       mainAxisAlignment: MainAxisAlignment.center,
-    //       children: <Widget>[
-    //         const Text(
-    //           'You have pushed the button this many times:',
-    //         ),
-    //         Text(
-    //           '$_counter',
-    //           style: Theme.of(context).textTheme.headline4,
-    //         ),
-    //       ],
-    //     ),
-    //   ),
-    //   floatingActionButton: FloatingActionButton(
-    //     onPressed: _incrementCounter,
-    //     tooltip: 'Increment',
-    //     child: const Icon(Icons.add),
-    //   ), // This trailing comma makes auto-formatting nicer for build methods.
-    // );
-// }
